1a87d178baeb92ca348cfa13f154bfad32ec92fa
RyuIlHan <https://github.com/inhatc/MyApplication>
2019-03-15 00:20:00 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './index.css';
+import {Provider} from 'react-redux';
+import store from '../src/store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+  ,
+  document.getElementById('root')
+);
